586d5c0289b004b4152b0bcbf9a56a49cc1d74b7
10Narratives <vanyatertyshnikov@yandex.ru>
2025-04-03 18:10:17 +0300

    Implement graceful shutdown for grpc server

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,26 +1,31 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/10Narratives/sso/internal/app"
 	"github.com/10Narratives/sso/internal/config"
 	"github.com/10Narratives/sso/internal/lib/logger/sl"
 )
 
 func main() {
-	// TODO: Make reading of configuration
 	cfg := config.MustLoad()
 
-	// TODO: Initialize logger
 	log := sl.SetupLogger(cfg.Env)
-
-	log.Info("Starting application")
-
-	// TODO: Initialize application
 
 	application := app.New(log, cfg)
 
-	application.GRPCApp.MustRun()
+	log.Info("starting application")
+	go application.GRPCApp.MustRun()
 
-	// TODO: run gRPC-server
-	log.Info("Stopping application")
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	<-stop
+
+	log.Info("stopping application")
+	application.GRPCApp.Stop()
+	log.Info("grpc server stopped")
 }
