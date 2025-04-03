d61300dfdb15a1b3267b6ac208bcaa26c510b4dd
10Narratives <vanyatertyshnikov@yandex.ru>
2025-04-03 08:11:19 +0300

    Implement logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"github.com/10Narratives/sso/internal/config"
+	"github.com/10Narratives/sso/internal/lib/logger/sl"
 )
 
 func main() {
 	// TODO: Make reading of configuration
-	_ = config.MustLoad()
+	cfg := config.MustLoad()
 
 	// TODO: Initialize logger
+	log := sl.SetupLogger(cfg.Env)
+
+	log.Info("Starting application")
 
 	// TODO: Initialize application
 
 	// TODO: run gRPC-server
+	log.Info("Stopping application")
 }
---
+++ internal/lib/logger/sl/sl.go
@@ -0,0 +1,51 @@
+package sl
+
+import (
+	"log"
+	"log/slog"
+	"os"
+
+	"github.com/10Narratives/sso/internal/lib/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func SetupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = setupPrettySlog()
+	case envDev:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default:
+		log.Fatal("Can not initialize logger: env parameter is unknown.")
+	}
+
+	return logger
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
+
+func Err(err error) slog.Attr {
+	return slog.Attr{
+		Key:   "error",
+		Value: slog.StringValue(err.Error()),
+	}
+}
